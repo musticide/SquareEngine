5656cfb37f3a6c3d25c56c12e7e79c4389100821
mustafa.khaizar <mustafa.khaizar@rampgroup.com>
2024-01-17 17:34:23 +0530

    Logging works!

--- Engine/src/EntryPoint.h
+++ Engine/src/EntryPoint.h
@@ -1,16 +1,26 @@
 #pragma once
 #include "Application.h"
+#include "Log.h"
+#include "spdlog/spdlog.h"
+#include <iostream>
+#include <spdlog/sinks/basic_file_sink.h>
 
 #ifdef SQ_PLATFORM_WINDOWS
 
-extern SqEngine::Application *SqEngine::CreateApplication();
+extern SqEngine::Application* SqEngine::CreateApplication();
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
+    printf("Hello Engine");
 
-  auto app = SqEngine::CreateApplication();
-  app->Run();
-  delete app;
-  return 0;
+    SqEngine::Log::Init();
+    SqEngine::Log::GetCoreLogger()->warn("Initialized Logger!!");
+    SqEngine::Log::GetClientLogger()->error("If you see this be happy!");
+
+    auto app = SqEngine::CreateApplication();
+    app->Run();
+    delete app;
+    return 0;
 }
 
 #endif
--- Engine/src/SqEngine.h
+++ Engine/src/SqEngine.h
@@ -5,4 +5,6 @@
 //--------ENTRY POINT------------//
 #include "EntryPoint.h"          //
 //-------------------------------//
+#include "Log.h"
 
+#include <stdio.h>
